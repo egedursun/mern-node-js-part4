e9c944c83415c6fe34a4620fb9c5be9c4a094206
egedursun <edogandursun@gmail.com>
2022-12-23 18:35:34 +0300

    Implemented and tested the requirements for the Exercises (4.5, 4.6, 4.7).

--- tests/list.test.js
+++ tests/list.test.js
@@ -71,76 +71,201 @@ describe("total likes", () => {
 
 describe("favorite blog metrics", () => {
 
-    //todo: define the test inputs
-    const listWithNoBlog = []
-    const listWithOneBlog = []
-    const listWithThreeBlogs = []
+    const listWithNoBlog = [
 
-    //todo: update the expectations for the tests
+    ]
+
+    const listWithOneBlog = [
+        {
+            _id: "5a422aa71b54a676234d17f8",
+            title: "Go To Statement Considered Harmful",
+            author: "Edsger W. Dijkstra",
+            url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+            likes: 5,
+            __v: 0
+        }
+    ]
+
+    const listWithThreeBlogs = [
+        {
+            _id: "5a422aa71b54a676234d17f7",
+            title: "This is a blog",
+            author: "Edsger W. Dijkstra",
+            url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+            likes: 18,
+            __v: 0
+        },
+        {
+            _id: "5a422aa71b54a676234d17f8",
+            title: "This is another blog",
+            author: "Edsger W. Dijkstra",
+            url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+            likes: 23,
+            __v: 0
+        },
+        {
+            _id: "5a422aa71b54a676234d17f9",
+            title: "This is the most liked blog",
+            author: "Edsger W. Dijkstra",
+            url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+            likes: 42,
+            __v: 0
+        }
+    ]
 
     test("when list with no blogs, favorite blog is empty", () => {
         const result = listHelper.favoriteBlog(listWithNoBlog)
-        expect(result).toBe({})
+        expect(result).toStrictEqual({})
     })
 
     test("when list has a single item, favorite blog is that one", () => {
         const result = listHelper.favoriteBlog(listWithOneBlog)
-        expect(result).toBe({})
+        expect(result).toStrictEqual({
+            title: "Go To Statement Considered Harmful",
+            author: "Edsger W. Dijkstra",
+            likes: 5
+        })
     })
 
     test("when list has multiple elements, favorite blog is the most liked one", () => {
         const result = listHelper.favoriteBlog(listWithThreeBlogs)
-        expect(result).toBe({})
+        expect(result).toStrictEqual({
+            title: "This is the most liked blog",
+            author: "Edsger W. Dijkstra",
+            likes: 42
+        })
     })
 })
 
 describe("author with most blogs", () => {
 
-    //todo: define the test inputs
-    const listWithNoBlog = []
-    const listWithOneBlog = []
-    const listWithThreeBlogs = []
+    const listWithNoBlog = [
 
-    //todo: update the expectations for the tests
+    ]
+
+    const listWithOneBlog = [
+        {
+            _id: "5a422aa71b54a676234d17f8",
+            title: "Go To Statement Considered Harmful",
+            author: "Edsger W. Dijkstra",
+            url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+            likes: 7,
+            __v: 0
+        }
+    ]
+
+    const listWithThreeBlogs = [
+        {
+            _id: "5a422aa71b54a676234d17f7",
+            title: "This is a blog",
+            author: "Ege",
+            url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+            likes: 50,
+            __v: 0
+        },
+        {
+            _id: "5a422aa71b54a676234d17f8",
+            title: "This is another blog",
+            author: "Mert",
+            url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+            likes: 23,
+            __v: 0
+        },
+        {
+            _id: "5a422aa71b54a676234d17f9",
+            title: "This is the most liked blog",
+            author: "Mert",
+            url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+            likes: 28,
+            __v: 0
+        }
+    ]
 
     test("when there is no blogs, author with most blogs is empty", () => {
         const result = listHelper.mostBlogs(listWithNoBlog)
-        expect(result).toBe({})
+        expect(result).toStrictEqual({})
     })
 
     test("when there is a single blog, the most blogging author is that blog", () => {
         const result = listHelper.mostBlogs(listWithOneBlog)
-        expect(result).toBe({})
+        expect(result).toStrictEqual({
+            author: "Edsger W. Dijkstra",
+            blogs: 1
+        })
     })
 
     test("when there are multiple blogs, the most blogging author is the one with most blogs", () => {
         const result = listHelper.mostBlogs(listWithThreeBlogs)
-        expect(result).toBe({})
+        expect(result).toStrictEqual({
+            author: "Mert",
+            blogs: 2
+        })
     })
 })
 
 
 describe("author with most liked blogs", () => {
 
-    //todo: define the test inputs
-    const listWithNoBlog = []
-    const listWithOneBlog = []
-    const listWithThreeBlogs = []
+    const listWithNoBlog = [
 
-    //todo: update the expectations for the tests
+    ]
+
+    const listWithOneBlog = [
+        {
+            _id: "5a422aa71b54a676234d17f8",
+            title: "Go To Statement Considered Harmful",
+            author: "Edsger W. Dijkstra",
+            url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+            likes: 7,
+            __v: 0
+        }
+    ]
+
+    const listWithThreeBlogs = [
+        {
+            _id: "5a422aa71b54a676234d17f7",
+            title: "This is a blog",
+            author: "Ege",
+            url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+            likes: 50,
+            __v: 0
+        },
+        {
+            _id: "5a422aa71b54a676234d17f8",
+            title: "This is another blog",
+            author: "Mert",
+            url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+            likes: 23,
+            __v: 0
+        },
+        {
+            _id: "5a422aa71b54a676234d17f9",
+            title: "This is the most liked blog",
+            author: "Mert",
+            url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+            likes: 28,
+            __v: 0
+        }
+    ]
 
     test("when there is no blogs, author with most liked blogs is empty", () => {
         const result = listHelper.mostLikes(listWithNoBlog)
-        expect(result).toBe({})
+        expect(result).toStrictEqual({})
     })
 
     test("when there is a single blog, the most liked author is the single one", () => {
         const result = listHelper.mostLikes(listWithOneBlog)
-        expect(result).toBe({})
+        expect(result).toStrictEqual({
+            author: "Edsger W. Dijkstra",
+            likes: 7
+        })
     })
 
     test("when there are multiple blogs, the most liked author is the one with most likes", () => {
         const result = listHelper.mostLikes(listWithThreeBlogs)
-        expect(result).toBe({})
+        expect(result).toStrictEqual({
+            author: "Mert",
+            likes: 51
+        })
     })
 })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -20,17 +20,102 @@ const totalLikes = (blogs) => {
 
 // EXERCISE (4.5)
 const favoriteBlog = (blogs) => {
-    //todo: find which blog has most likes, return that blog
+    if (blogs.length === 0){
+        return {}
+    } else if (blogs.length === 1){
+        return {
+            title: blogs[0].title,
+            author: blogs[0].author,
+            likes: blogs[0].likes
+        }
+    } else {
+        let mostLikes = blogs[0].likes
+        let mostLikedBlog = blogs[0]
+        for (let i = 0; i < blogs.length; i++){
+            if (blogs[i].likes > mostLikes){
+                mostLikes = blogs[i].likes
+                mostLikedBlog = blogs[i]
+            }
+        }
+        return {
+            title: mostLikedBlog.title,
+            author: mostLikedBlog.author,
+            likes: mostLikedBlog.likes
+        }
+    }
 }
 
 // EXERCISE (4.6)
 const mostBlogs = (blogs) => {
-    //todo: find the author which has the most amount of blogs
+    if (blogs.length === 0){
+        return {}
+    } else if (blogs.length === 1){
+        return {
+            author: blogs[0].author,
+            blogs: 1
+        }
+    } else {
+        let authorBlogs = {}
+        for (let i = 0; i < blogs.length; i++){
+            let author = blogs[i].author
+            let sum = 0
+            for (let j = 0; j < blogs.length; j++){
+                if (blogs[j].author === author){
+                    sum += 1
+                }
+            }
+            authorBlogs[author] = sum
+        }
+
+        let maximum = 0
+        let maximumAuthor = ""
+        for (let item in authorBlogs){
+            if (authorBlogs[item] > maximum){
+                maximum = authorBlogs[item]
+                maximumAuthor = item
+            }
+        }
+        return {
+            author: maximumAuthor,
+            blogs: maximum
+        }
+    }
 }
 
 // EXERCISE (4.7)
 const mostLikes = (blogs) => {
-    //todo: find the author, which has the most amount of likes for the blogs
+    if (blogs.length === 0){
+        return {}
+    } else if (blogs.length === 1){
+        return {
+            author: blogs[0].author,
+            likes: blogs[0].likes
+        }
+    } else {
+        let authorBlogs = {}
+        for (let i = 0; i < blogs.length; i++){
+            let author = blogs[i].author
+            let sum = 0
+            for (let j = 0; j < blogs.length; j++){
+                if (blogs[j].author === author){
+                    sum += blogs[j].likes
+                }
+            }
+            authorBlogs[author] = sum
+        }
+        let maximum = 0
+        let maximumAuthor = ""
+        for (let item in authorBlogs){
+            if (authorBlogs[item] > maximum){
+                maximum = authorBlogs[item]
+                maximumAuthor = item
+            }
+        }
+        return {
+            author: maximumAuthor,
+            likes: maximum
+        }
+    }
 }
 
 module.exports = {
