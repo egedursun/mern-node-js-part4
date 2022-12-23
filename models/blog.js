b8467915c9da79a960e0c40ce55f94f8053ac84b
egedursun <edogandursun@gmail.com>
2022-12-23 16:44:34 +0300

    Implemented the new features for the exercises (4.1, 4.2).

---
+++ models/blog.js
@@ -0,0 +1,42 @@
+
+const mongoose = require("mongoose")
+require("dotenv").config()
+
+const url = process.env.MONGODB_URI
+
+console.log("Connecting to: ", url)
+
+mongoose.connect(url).then(() => {
+    console.log("Connected to MongoDB successfully")
+}).catch(error => {
+    console.log("There has been an error while connecting MongoDB: ", error.message)
+})
+
+const blogSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        minLength: 3,
+        required: true
+    },
+    author: {
+        type: String,
+        minLength: 3,
+        required: true
+    },
+    url: {
+        type: String,
+        minLength: 3,
+        required: true
+    },
+    likes: Number
+})
+
+blogSchema.set("toJSON", {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model("Blog", blogSchema)
